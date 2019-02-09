dbaa03efcb8430dfc8c7a3646323d91368cf6b58
kr <gairoo@hotmail.com>
2019-02-09 14:54:55 -0600

    yet another

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,35 +5,44 @@ with open('budget_data.csv','r') as rf:
 
 	csv_reader = csv.DictReader(rf)
 
-	great_incre =0
-	great_decre = 0
+	
 	total_month =0
 	total_pro_loss = 0
 	ave_chg=[]
+	great_date=[]
 	
 	for i in csv_reader:
 	
 		total_month +=1
 		total_pro_loss += int(i['Profit/Losses'])
 		ave_chg.append(int(i['Profit/Losses']))
-
-		if int(i['Profit/Losses']) > great_incre:
-			great_incre = int(i['Profit/Losses']) 
-			great_inc_date = i['Date']
-
-		if int(i['Profit/Losses']) < great_decre:
-			great_decre = int(i['Profit/Losses'])
-			great_dec_date = i['Date']
+		great_date.append(i['Date'])
+		
 	
 	
 	average_ch=[]
+
 	i=1
+
 	while i < len(ave_chg):
+
 		ave_chg_count = ave_chg[i] - ave_chg[i-1]
 		average_ch.append(ave_chg_count)
 		i+=1
 
 	ave_chg_final = sum(average_ch)/len(average_ch)
+
+	great_incre = max(average_ch)
+	great_decre = min(average_ch)
+
+	new_list = zip(average_ch,great_date[1:])
+
+	for i in new_list:
+		if i[0] == great_incre:
+			great_inc_date =i[1]
+		if i[0] == great_decre:
+			great_dec_date =i[1]
+
 
 	
 with open('budget_data.txt','w') as wf:
