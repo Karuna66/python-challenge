aca52730ad71cd7dba9a33d4097134702518c2ed
kr <gairoo@hotmail.com>
2019-02-09 13:46:33 -0600

    this is my commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,12 +9,13 @@ with open('budget_data.csv','r') as rf:
 	great_decre = 0
 	total_month =0
 	total_pro_loss = 0
-
+	ave_chg=[]
 	
 	for i in csv_reader:
 	
 		total_month +=1
 		total_pro_loss += int(i['Profit/Losses'])
+		ave_chg.append(int(i['Profit/Losses']))
 
 		if int(i['Profit/Losses']) > great_incre:
 			great_incre = int(i['Profit/Losses']) 
@@ -23,16 +24,25 @@ with open('budget_data.csv','r') as rf:
 		if int(i['Profit/Losses']) < great_decre:
 			great_decre = int(i['Profit/Losses'])
 			great_dec_date = i['Date']
-		
-		
+	
+	
+	average_ch=[]
+	i=1
+	while i < len(ave_chg):
+		ave_chg_count = ave_chg[i] - ave_chg[i-1]
+		average_ch.append(ave_chg_count)
+		i+=1
 
+	ave_chg_final = sum(average_ch)/len(average_ch)
+
+	
 with open('budget_data.txt','w') as wf:
 		
 			wf.write('Financial Analysis\n')
 			wf.write("............................................\n")
 			wf.write(f'Total month: {total_month}\n')
 			wf.write(f'Total: ${total_pro_loss}\n')
-			wf.write(f'Average Change : ${total_pro_loss/total_month:.02f}\n')
+			wf.write(f'Average Change : ${ave_chg_final:.02f}\n')
 			wf.write(f'Greatest Increase in Profits: {great_inc_date}  (${great_incre})\n')
 			wf.write(f'Greatest Decrease in Profits: {great_dec_date}  (${great_decre})\n')
 
@@ -40,6 +50,6 @@ with open('budget_data.txt','w') as wf:
 			print("............................................")
 			print(f'Total month: {total_month}')
 			print(f'Total: ${total_pro_loss}')
-			print(f'Average Change :  ${total_pro_loss/total_month:.02f}')
+			print(f'Average Change :  ${ave_chg_final:.02f}')
 			print(f'Greatest Increase in Profits: {great_inc_date}  (${great_incre})')
 			print(f'Greatest Decrease in Profits: {great_dec_date}  (${great_decre})')
